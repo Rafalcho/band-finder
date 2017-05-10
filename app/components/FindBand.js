3d1054dc337817d4e0b5d9abbc4f1d0274a99302
Rafalcho <raf.cholaj@gmail.com>
2017-05-10 15:54:45 +0200

    basic fetch

--- app/components/FindBand.js
+++ app/components/FindBand.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import {artistSearch} from '../utils/api';
 
 class FindBand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      artist: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      artist: event.currentTarget.value
+    });
+
+  };
+
   render() {
     return (
       <div className='search-container'>
         <h1>Find your favourite band or artist</h1>
-        <input type='text' />
-        <button>Go!</button>
+        <input
+          type='text'
+          value={this.state.artist}
+          onChange={this.handleChange}/>
+        <button onClick={() => artistSearch(this.state.artist)}>Go!</button>
       </div>
     );
   }
---
+++ app/utils/api.js
@@ -0,0 +1,19 @@
+export const artistSearch = (artist) => {
+  // const artistString = [...artist].join('%20');
+  const url = `https://api.spotify.com/v1/search?q=${artist}&type=artist`;
+  const searchFetch = fetch(url);
+
+  searchFetch.then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Fetching failed');
+    }
+  })
+  .then(response => {
+    console.log(response.artists.items[0]);
+  })
+  .catch(error => {
+    console.log('Fetching data error:', error);
+  });
+};
