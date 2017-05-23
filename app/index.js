61472138ac5815dab120a09cf7226d0c286a4d17
Rafalcho <raf.cholaj@gmail.com>
2017-05-23 22:00:06 +0200

    images now load correctly

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 require('./index.css');
+require('./images/background.jpeg');
+require('./images/darth_stripe.png');
 
 ReactDOM.render(
   <App />, document.getElementById('app')
